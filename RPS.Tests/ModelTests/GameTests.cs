0ffdf7342267b6d238cef37e3a5f964baa40f251
Connor Hansen <chansen13@georgefox.edu>
2021-09-22 13:28:16 -0700

    add test for a Draw between players.
    Co-authored-by: Anna Clarke <annac.klingberg@gmail.com>

--- RPS.Tests/ModelTests/GameTests.cs
+++ RPS.Tests/ModelTests/GameTests.cs
@@ -3,3 +3,15 @@ using System.Collections.Generic;
 using RPS.Models;
 using System;
 
+namespace RPS.Tests
+{
+    [TestClass]
+    public class GameTests
+    {
+      [TestMethod]
+      public void CheckWinner_CheckDraw_Draw()
+      {
+        Assert.AreEqual("Draw", Game.CheckWinner("rock", "rock"));
+      }
+    }
+}
--- RPS/Models/Game.cs
+++ RPS/Models/Game.cs
@@ -1,3 +1,25 @@
 using System;
 using System.Collections.Generic;
 
+
+namespace RPS.Models
+{
+    public class Game
+    {
+      public static string CheckWinner(string choice1, string choice2)
+      {
+        if (choice1 == choice2)
+        {
+          return "Draw";
+        }
+        else
+        {
+          return "Not yet";
+        }
+      }
+    }
+}
+
+// CheckWinner takes 2 inputs (rock, scissors)
+// if !same, there is a winner (else draw)
+// branching for r>s, s>p, p>r
