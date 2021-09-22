a384bc6cda3a0260fd22bc5b03e95c2032b52542
Connor Hansen <chansen13@georgefox.edu>
2021-09-22 14:11:17 -0700

    add console UI for RPS game.

---
+++ README.md
@@ -0,0 +1,41 @@
+# Rock Paper Scissors
+
+#### This console application will ask the user for two different choices and paly a game of rock paper scissors.
+
+#### By Connor Hansen and Anna Clarke
+
+## Technologies Used
+
+* C#
+* .Net
+* MStest
+
+## Description
+
+_This application will ask the user for two inputs from two different players and play a game of rock, paper, scissors. It will display the winner or draw in the console._
+
+## Setup/Installation Requirements
+
+- _To clone and run from [GitHub Repo](https://github.com/chansen1395/Rock-Paper-Scissors-9-22):_
+
+- _Use a program such as VSCode to view and run the program._
+  - _In bash at a folder of your choice, type the following:_
+    - $ git clone https://github.com/chansen1395/Rock-Paper-Scissors-9-22
+    - $ code .
+    - $ dotnet restore
+    - $ dotnet run
+
+
+## Known Bugs
+
+* _No known bugs_
+
+## License
+
+{Let me know if you run into any issues or have questions, ideas or concerns. I encourage you to contact me or make a contribution to the code.}
+
+## Contact Information
+
+- Anna Clarke: _{<anclarkie@gmail.com>}_
+- _{<chansen13@georgefox.edu>}_
+- _{[GitHub Repo - main](https://github.com/chansen1395/Rock-Paper-Scissors-9-22)}_
--- RPS/Program.cs
+++ RPS/Program.cs
@@ -1,2 +1,44 @@
 using System;
 using System.Collections.Generic;
+using RPS.Models;
+
+
+namespace RPS
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Welcome to Rock, Paper, Scissors!");
+      Console.WriteLine("Player 1: [rock/paper/scissors] [q to quit]");
+      string choice1 = Console.ReadLine().ToLower();
+      Console.WriteLine("Player 2: [rock/paper/scissors] [q to quit]");
+      string choice2 = Console.ReadLine().ToLower();
+      string[] validPlays = new string[] { "rock", "paper", "scissors" };
+      
+      if (choice1 == "q" || choice2 == "q")
+      {
+        Console.WriteLine("Goodbye");
+      } else if (Array.Exists(validPlays, element => element == choice1) && Array.Exists(validPlays, element => element == choice2))
+      {
+        string result = Game.CheckWinner(choice1, choice2);
+        if (result == "Draw")
+        {
+          Console.WriteLine("It was a Draw");
+          Console.WriteLine("--------");
+          Main();
+        } else
+        {
+        Console.WriteLine("The winner is " + result);
+        Console.WriteLine("--------");
+        Main();
+        }
+      } else 
+      {
+        Console.WriteLine("Please enter rock, paper, or scissors");
+        Console.WriteLine("--------");
+        Main();
+      }
+    }
+  }
+}
